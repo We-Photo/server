7e92dc77448aa7cee72fd2a3cf1777b192f0bff5
joon <khjoon372@naver.com>
2024-12-30 16:04:09 +0900

    first commit

---
+++ src/main/java/com/java/wephoto/WephotoApplication.java
@@ -0,0 +1,13 @@
+package com.java.wephoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WephotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WephotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/wephoto/WephotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.wephoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WephotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
